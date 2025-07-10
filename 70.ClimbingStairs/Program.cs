c50ce55dddcc13d9d4e5305b55427f481d13562b
Abdulbaki Deveci <abdulbaki.deveci0027@gmail.com>
2025-07-10 21:31:41 +0300

    new

---
+++ 70.ClimbingStairs/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+public class Solution
+{
+    public int ClimbStairs(int n)
+    {
+        int[] array = new int[n];
+        int returnValue = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+            {
+                array[0] = 1;
+                returnValue = array[0];
+            }
+            else if (i == 1)
+            {
+                array[i] = array[0] + i;
+                returnValue = array[i];
+            }
+            else
+            {
+                array[i] = array[i - 1] + array[i - 2];
+                returnValue = array[i];
+            }
+
+        }
+        return returnValue;
+    }
+}
